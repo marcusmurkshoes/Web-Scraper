086730b523557b802315725fc131f4733aba35a1
Marcus <therealthug09@aol.com>
2018-04-06 12:02:57 -0600

    second deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/articlescraper";
 // Initialize Express
 var app = express();
